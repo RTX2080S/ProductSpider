1a7455b3f6b7137999c9f4b4985b7fd406415826
LL <52593730@qq.com>
2017-06-10 22:39:51 +1200

    Put params to app.config files

--- ProductSpider/Program.cs
+++ ProductSpider/Program.cs
@@ -6,11 +6,13 @@ namespace IMProductSpider
     class Program
     {
         const string ProductDetailsUrl_Config_Key = "ProductDetailsUrl";
-        
+        const string UrlParams_Config_Key = "UrlParams";
+
         static void Main(string[] args)
         {
             string ProductDetailsUrl = ConfigurationManager.AppSettings.Get(ProductDetailsUrl_Config_Key);
-            Console.WriteLine(ProductDetailsUrl);
+            string UrlParams = ConfigurationManager.AppSettings.Get(UrlParams_Config_Key);
+            Console.WriteLine($"{ProductDetailsUrl}{UrlParams}");
             Console.ReadLine();
         }
     }
