f33a1fc6a7d73fa89e6043ee2c189bc2be3d3fba
LL <52593730@qq.com>
2017-06-11 21:06:14 +1200

    refactor stage 1

--- ProductSpider.ConsoleApp/Program.cs
+++ ProductSpider.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using ProductSpider.Models;
 using ProductSpider.Services;
+using ProductSpider.Clients;
 
 namespace ProductSpider.ConsoleApp
 {
@@ -16,29 +17,9 @@ namespace ProductSpider.ConsoleApp
             string UrlParams = ConfigurationManager.AppSettings.Get(UrlParams_Config_Key);
             int dummySKU = 3392393;
 
-            var skuFormatter = new SKUFormatter();
-            var formattedSKU = skuFormatter.GetFormattedSKUString(dummySKU);
-
-            var productUrl = $"{ProductDetailsUrl}{UrlParams}{formattedSKU}";
-            var httpClient = new HttpClientService();
-
-            Console.WriteLine($"Downloading content -> {productUrl}...");
-
-            var downloadTask = httpClient.DownloadDocumentAsyn(productUrl);
-            downloadTask.Wait();
-
-            var content = downloadTask.Result;
-
-            var contentReader = new ContentReader();
-            contentReader.SetContext(content);
-
-            var productDetails = new ProductDetails();
-            productDetails.Url = productUrl;
-            productDetails.Title = contentReader.ReadContent("<title>", " - Product Details").Trim();
-            productDetails.ImageUrl = contentReader.ReadContent("<a class=\"change-cursor\" href=\"", "\">").Trim();
-
-            Console.WriteLine("Done! ");
-
+            var productSpider = new IMProductSpider(ProductDetailsUrl, UrlParams);
+            var productResult = productSpider.GetProductDetailsBySKU(dummySKU);
+            
             Console.ReadLine();
         }
     }
