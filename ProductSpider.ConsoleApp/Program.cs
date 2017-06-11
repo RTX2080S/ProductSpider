ca6a1623b58cb1db1419b0e15aa736265778f046
LL <52593730@qq.com>
2017-06-11 22:14:15 +1200

    make the process parallel

--- ProductSpider.ConsoleApp/Program.cs
+++ ProductSpider.ConsoleApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Configuration;
-using ProductSpider.Clients;
+using System.Threading.Tasks;
 using System.Collections.Generic;
+using ProductSpider.Clients;
 using ProductSpider.Models;
 
 namespace ProductSpider.ConsoleApp
@@ -14,18 +15,29 @@ namespace ProductSpider.ConsoleApp
             string UrlParams = ConfigurationManager.AppSettings.Get(ConfigKeys.UrlParams_Config_Key);
             string inputFile = ConfigurationManager.AppSettings.Get(ConfigKeys.Input_File_Config_Key);
             string outputFile = ConfigurationManager.AppSettings.Get(ConfigKeys.Output_File_Config_Key);
-            
+
+            Console.WriteLine("Application initializing...");
+
+            Console.WriteLine($"Loading input file {inputFile}...");
             var skuReader = new SkuReader();
             var skuList = skuReader.Load(inputFile);
-            
+
             var productDetails = new List<ProductDetails>();
             var productSpider = new IMProductSpider(ProductDetailsUrl, UrlParams);
-            foreach (var sku in skuList)
+
+            Console.WriteLine("Getting product details...");
+            object lockResults = new object();
+            Parallel.ForEach(skuList, sku =>
             {
                 var productResult = productSpider.GetProductDetailsBySKU(sku);
-                productDetails.Add(productResult);
-            }
+                lock (lockResults)
+                {
+                    productDetails.Add(productResult);
+                    Console.WriteLine($"Get {sku} complete. ");
+                }
+            });
 
+            Console.WriteLine($"Saving CSV file {outputFile}...");
             var csvWriter = new CsvWriter();
             csvWriter.Save(outputFile, productDetails);
 
